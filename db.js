2c7a17693f11602cad08e138698c9da45c7705dc
Subin Lee <isubin0513@gmail.com>
2026-05-14 20:52:35 +0900

    fix: node:sqlite 내장 모듈로 교체, ranking 목데이터 제거

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
-const Database = require('better-sqlite3');
-const db = new Database('ranking.db');
+const { DatabaseSync } = require('node:sqlite');
+const db = new DatabaseSync('ranking.db');
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS scores (
